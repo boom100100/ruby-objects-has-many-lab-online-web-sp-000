807bce7e5081f2f83f00bf17401a63673a5271bf
Bernadette Davis <bsd231@nyu.edu>
2019-09-21 00:43:30 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,6 +1,16 @@
 class Post
   attr_accessor :title
   @@all = []
+
+  def initialize(title)
+    @title = title
+    save
+  end
+
+  def save
+      @@all << self
+  end
+  
   def all
     @@all
   end
